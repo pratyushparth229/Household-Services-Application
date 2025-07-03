8f682b8b55fd8c183b6ca62d6a066ea870ca409c
pratyushparth229 <128896910+pratyushparth229@users.noreply.github.com>
2025-07-03 20:03:52 +0530

    Update app.py

--- Code/app.py
+++ Code/app.py
@@ -1550,7 +1550,12 @@ def generate_charts(labels_role, values_role, labels_request, values_request):
         pass
 
 
-if __name__=="__main__":
-    init_db()
-    app.run(debug=True)
- 
+# if __name__=="__main__":
+#     init_db()
+#     app.run(debug=True)
+
+# import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Use the PORT provided by Render
+    app.run(host="0.0.0.0", port=port)
